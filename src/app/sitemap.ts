e8353f4e710d741be71c1436762d11f48d90939f
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2024-11-03 13:31:49 +0900

    [feat] add sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+// ref: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
+
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://yuto-kimura-g.github.io',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
