34c6d42a6546bec6a78bb19633c235a99a2ef593
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2024-11-06 14:19:29 +0900

    [add] robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+// ref: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://yuto-kimura-g.github.io/sitemap.xml',
+  }
+}
