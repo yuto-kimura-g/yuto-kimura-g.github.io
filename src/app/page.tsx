5f88215deef025d541bc985d94b167b4c3717c1a
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2025-10-07 18:20:57 +0900

    [update] contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,26 @@
 import ListItem from '@/components/ListItem';
 import HrefText from '@/components/HrefText';
-// import Dummy from '@/components/Dummy';
 
 export default function IndexPage() {
   return (
-    <div className="w-full space-y-10 bg-theme-light-bg bg-opacity-50 px-5 py-16 lg:px-16">
-      <div>
-        <div className="text-3xl font-bold">About</div>
-        <ul>
-          <ListItem title="Name" body={<span>Yuto KIMURA</span>} />
-          <ListItem
-            title="Affiliation"
-            body={
-              <HrefText
-                text="GSoIS, University of Hyogo"
-                link="https://u-hyogo.info/"
-              />
-            }
-          />
-          <ListItem
-            title="Contact"
-            body={<span>ad24g018 [at] guh [dot] u-hyogo.ac.jp</span>}
-          />
-          <ListItem
-            title="Interest"
-            body={
-              <span>
-                <span className="underline">Mathematical Optimization</span>{' '}
-                (real-world application: steel industry, container terminal),{' '}
-                <span className="underline">Software Development</span>{' '}
-                (Full-stack, OSS, Container, Cloud, Software Architecture), ...
-              </span>
-            }
-          />
-        </ul>
-      </div>
-      <div>
-        <div className="text-3xl font-bold">Link</div>
-        <ul>
-          <ListItem
-            title="GitHub"
-            body={<HrefText link="https://github.com/yuto-kimura-g/" />}
-          />
-          <ListItem
-            title="Website"
-            body={
-              <HrefText link="https://github.com/yuto-kimura-g/yuto-kimura-g.github.io/" />
-            }
-          />
-          <ListItem
-            title="Weblog"
-            body={<HrefText link="https://md-weblog.pages.dev/" />}
-          />
-          <ListItem
-            title="Ocean View"
-            body={
-              <HrefText link="https://yuto-kimura-g.github.io/ocean-view/" />
-            }
-          />
-        </ul>
-      </div>
-      {/* <Dummy /> */}
+    <div className="w-full bg-theme-light-bg bg-opacity-50 px-5 py-16 lg:px-16">
+      <div className="text-2xl font-bold">About</div>
+      <ul>
+        <ListItem title="Name" body={<span>Yuto KIMURA</span>} />
+        <ListItem
+          title="Affiliation"
+          body={
+            <HrefText
+              text="Graduate School of Information Science, University of Hyogo"
+              link="https://u-hyogo-gsis.org/"
+            />
+          }
+        />
+        <ListItem
+          title="Contact"
+          body={<span>ad24g018 [at] guh [dot] u-hyogo.ac.jp</span>}
+        />
+      </ul>
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="flex h-[10vh] w-full items-center justify-center bg-theme-light-bg bg-opacity-50">
       <div className="space-x-1">
-        <span>&copy; 2024</span>
+        <span>&copy; {year}</span>
         <a href="https://github.com/yuto-kimura-g/">yuto-kimura-g.</a>
         <span>All rights reserved.</span>
       </div>
