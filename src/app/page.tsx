92a72b42c6c4a1a281beddc8e53539d588aa0bb5
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2025-10-18 21:35:17 +0900

    [update] contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
-import ListItem from '@/components/ListItem';
 import HrefText from '@/components/HrefText';
 
 export default function IndexPage() {
   return (
-    <div className="w-full bg-theme-light-bg bg-opacity-50 px-5 py-16 lg:px-16">
+    <div className="bg-theme-light-bg bg-opacity-50 w-full px-5 py-16 lg:px-16">
       <div className="text-2xl font-bold">About</div>
       <ul>
-        <ListItem title="Name" body={<span>Yuto KIMURA</span>} />
-        <ListItem
-          title="Affiliation"
-          body={
-            <HrefText
-              text="Graduate School of Information Science, University of Hyogo"
-              link="https://u-hyogo-gsis.org/"
-            />
-          }
-        />
-        <ListItem
-          title="Contact"
-          body={<span>ad24g018 [at] guh [dot] u-hyogo.ac.jp</span>}
-        />
+        <li>
+          Name: Yuto KIMURA
+        </li>
+        <li>
+          Affiliation: <HrefText
+            text="Graduate School of Information Science, University of Hyogo"
+            link="https://u-hyogo-gsis.org/"
+          />
+        </li>
+        <li>
+          Contact: ad24g018 [at] guh [dot] u-hyogo.ac.jp
+        </li>
       </ul>
-    </div>
+    </div >
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,9 @@
-import Link from 'next/link';
-
 export const Header = () => {
   return (
     <header className="flex h-[10vh] items-center bg-theme-light-bg bg-opacity-50">
-      <nav className="mx-auto flex w-[90vw] items-center">
-        <Link href={'/'} className="flex items-center">
-          <img src="/y.png" alt="icon" className="h-[8vh]" />
-          <span className="text-2xl">yuto : : website</span>
-        </Link>
+      <nav className="relative mx-auto flex w-[90vw] items-center">
+        <img src="/y.png" alt="icon" className="h-[8vh]" />
+        <span className="text-2xl">yuto : : website</span>
       </nav>
     </header>
   );
